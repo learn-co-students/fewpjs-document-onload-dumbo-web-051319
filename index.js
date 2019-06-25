decd7fbdc525da0a6d7a647c9095bb415d8e4c6c
Stefanos <stefanosu422@gmail.com>
2019-06-25 19:26:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+
+
+
+const changeDom = document.addEventListener("DOMContentLoaded", () => {
+  updateDOM() 
+})
+
+
+const updateDOM = () => {
+  document.getElementById("text").innerHTML = ("This is really cool!")
+} 
+
+
+
+
+
+
+ 
