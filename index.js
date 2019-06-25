5b47fe97560f5697001044bc0c4916decee2662e
Chloe <gingkolane@gmail.com>
2019-06-25 15:53:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+
+
+document.addEventListener("DOMContentLoaded", changeText);
+
+function changeText() {
+  document.getElementById("text").textContent = "This is really cool!";
+};
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   console.log("The DOM has loaded");
+//   document.getElementById("text").textContent = "This is really cool!";
+// });
+
+
