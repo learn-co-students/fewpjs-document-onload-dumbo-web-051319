00df8113e7e5cfedda5fc34b1e60cc617f42a15f
Yonas Fesehatsion <yone920@gmail.com>
2019-06-26 11:48:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+
+// console.log(
+//     "Should be first"
+//   );
+// debugger
+
+document.addEventListener("DOMContentLoaded", function() {
+    // console.log("The DOM has loaded");
+    // debugger
+   
+  });
+
+  let text2 = document.querySelector("#text2")
+    text2.textContent = "This past should load second!!!!!!!!"
+//   console.log(
+//     "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+//   );
