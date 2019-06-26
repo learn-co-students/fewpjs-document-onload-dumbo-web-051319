e7de50c091508260c565cbb6a4fb6aca4121fe93
Joe Jamsky <cba1067950@yahoo.com>
2019-06-25 23:32:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
   var pTag = document.getElementById('text');
-  pTag.innerText = "This is really cool!";
-  console.log("The DOM has loaded");
+  pTag.innerHTML = "This is really cool!";
+  // console.log("The DOM has loaded");
 });
